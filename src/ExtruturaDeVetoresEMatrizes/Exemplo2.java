0945feb6473ea024fc63a633e91dbeb41316e656
a9210103 <a9210103@02LAB108PC05.rn.senac.br>
2016-06-21 18:28:10 -0300

    lista5

--- src/ExtruturaDeVetoresEMatrizes/Exemplo2.java
+++ src/ExtruturaDeVetoresEMatrizes/Exemplo2.java
@@ -8,30 +8,28 @@ public class Exemplo2 {
     
     Scanner ent = new Scanner(System.in);
 
-// Minha versão    
-//
-//    String nome[] = new String[3];
-//    int nota[][] = new int[3][4];
-//    int soma[] = new int[3];
-//       
-//    for (int i = 0; i <nome.length; i++){
-//        
-//        System.out.print("Nome " + (i+1) + ": ");
-//        nome[i] = ent.next();
-//    
-//        for (int j = 0; j < 4; j++) {
-//            System.out.print("Notas " + (j+1) + " de " + nome[i] + ": ");
-//            nota[i][j] = ent.nextInt();
-//            if ((nota[i][j] < 0) || (nota[i][j] > 10)) j--;
-//                else soma[i] = soma[i] + nota[i][j];
-//        }
-//            System.out.println("A média de " + nome[i] + " é: " + (soma[i]/4));     
-//            if ((soma[i]/4) >= 7){System.out.println(" APROVADO !!!");
-//            }
-//               else {System.out.println("Reprovado !!!");
-//               }
-//     
-//    }  
+    // Minha versão    
+    String nome[] = new String[3];
+    int nota[][] = new int[3][4];
+    int soma[] = new int[3];
+       
+    for (int i = 0; i <nome.length; i++){
+        
+        System.out.print("Nome " + (i+1) + ": ");
+        nome[i] = ent.next();
+    
+        for (int j = 0; j < 4; j++) {
+            System.out.print("Notas " + (j+1) + " de " + nome[i] + ": ");
+            nota[i][j] = ent.nextInt();
+            if ((nota[i][j] < 0) || (nota[i][j] > 10)) j--;
+                else soma[i] = soma[i] + nota[i][j];
+        }
+            System.out.println("A média de " + nome[i] + " é: " + (soma[i]/4));     
+            if ((soma[i]/4) >= 7){System.out.println(" APROVADO !!!");
+            }
+               else {System.out.println("Reprovado !!!");
+               }
+    }  
     
 
     
@@ -67,30 +65,30 @@ public class Exemplo2 {
     
     
 // Exemplo 2 Professor    
-
-        String nomes[] = new String[2];
-        int notas[][] = new int[2][3];
-    
-    for (int i = 0; i <nomes.length; i++){
-        
-        System.out.print("Nome " + (i+1) + ": ");
-        nomes[i] = ent.next();
-    
-        for (int j = 0; j < 3; j++) {
-            System.out.print("Notas " + (j+1) + " de " + nomes[i] + ": ");
-            notas[i][j] = ent.nextInt();
-        }
-    }
-    
-    for (String nome : nomes){
-        
-        System.out.print(nome);
-        
-        for (int[] nota : notas){
-            
-            System.out.print(nota);
-        }    
-   }   
+// Não concluído
+//        String nomes[] = new String[2];
+//        int notas[][] = new int[2][3];
+//    
+//    for (int i = 0; i <nomes.length; i++){
+//        
+//        System.out.print("Nome " + (i+1) + ": ");
+//        nomes[i] = ent.next();
+//    
+//        for (int j = 0; j < 3; j++) {
+//            System.out.print("Notas " + (j+1) + " de " + nomes[i] + ": ");
+//            notas[i][j] = ent.nextInt();
+//        }
+//    }
+//    
+//    for (String nome : nomes){
+//        
+//        System.out.print(nome);
+//        
+//        for (int[] nota : notas){
+//            
+//            System.out.print(nota);
+//        }    
+//   }   
 
 //        nome[i] = ent.next();
 //    
